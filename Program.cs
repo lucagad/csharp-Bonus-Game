1917469ab92c1f8ab291fc4e248a29d7d67530e3
Luca Gaddini <lucagaddini@icloud.com>
2022-09-17 16:07:12 +0200

    Fattoriale

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+// CALCOLO FATTORIALE DI UN NUMERO
+
+Console.WriteLine(fattoriale(5));
+
+int fattoriale(int n)
+{
+    if (n == 1) return 1;
+
+    else
+    {
+        return n = n * fattoriale(n - 1);
+    }
+}
